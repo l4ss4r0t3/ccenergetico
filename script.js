9b5e72257f2298dc9799433d1965446a91699894
Caio Ronald Carlos da Silva <l4ss4r0t3@gmail.com>
2026-04-02 19:57:28 -0300

    atualiza o script.js
    - adiciona as instruções para o resultado do Custo

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 const consumo = document.getEmelentById("consumo");
+const preco = document.getElementById("preco");
+const rCusto = document.getElementById("rCusto");
+const cBtn2 = document.getElementById("cBtn2");
+
+function cCusto(cBtn2){
+innerHtml.rCusto(consumo + preco)
+};
