6ecf525f4d803c52546236a1f478024b19024601
Caio Ronald Carlos da Silva <l4ss4r0t3@gmail.com>
2026-04-02 20:36:47 -0300

    atualiza o script.js
    - corrige todos os erros de sintaxe e digitação

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ const preco = document.getElementById("preco");
 const rCusto = document.getElementById("rCusto");
 const cBtn2 = document.getElementById("cBtn2");
 
-cBtn2.addEventListener("onClick, () => {rCusto.innerHTML = consumo + preco});
+cBtn2.addEventListener("click", () => {
+const vConsumo = consumo.value;
+const vPreco = preco.value;
+rCusto.innerHTML = "Esse consumo vai lhe custar: R$ " + Number(vConsumo) * Number(vPreco) + "!";
+});
