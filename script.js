b678c6b5a5468b84723975d6fa20fdb93b02f4af
Caio Ronald Carlos da Silva <l4ss4r0t3@gmail.com>
2026-04-02 20:06:29 -0300

    atualizar o script.js
    - corrige erros de digitação

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const consumo = document.getEmelentById("consumo");
+const consumo = document.getElementById("consumo");
 const preco = document.getElementById("preco");
 const rCusto = document.getElementById("rCusto");
 const cBtn2 = document.getElementById("cBtn2");
 
 function cCusto(cBtn2){
-innerHtml.rCusto(consumo + preco)
+rCusto.innerHTML(consumo + preco)
 };
